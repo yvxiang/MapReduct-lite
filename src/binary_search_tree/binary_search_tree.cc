0193fc52bfce7a544e3558cc131c5f694fd7400a
yvxiang <linyvxiang@gmail.com>
2014-11-07 14:27:12 +0800

    add implemention of binary search tree, not apply to mapreduce_lite.cc yet

---
+++ src/binary_search_tree/binary_search_tree.cc
@@ -0,0 +1,38 @@
+#include "src/binary_search_tree/binary_search_tree.h"
+#include <string>
+
+bool BinarySearchTree::find(const std::string &key)
+{
+	if(!data_)
+		return false;
+	if(data_->get_key() == key) {
+		return true;
+	} else if(data_->get_key() < key) {
+		return left_child_->find(key);
+	} else {
+		return right_child_->find(key);
+	}
+}
+	
+void BinarySearchTree::insert(std::string &key, void *value)
+{
+	if(this->empty()) {
+		data_ = new DataNode();
+		data_->set_key(key);
+		data_->set_value(value);
+		return ;
+	}
+
+	BinarySearchTree *cur_node = this;
+	while(cur_node && !cur_node->empty()) {
+		if(cur_node->data() > key) {
+			cur_node = cur_node->get_lchild();
+		} else {
+			cur_node = cur_node->get_rchild();
+		}
+	}
+	if(!cur_node)
+		cur_node = new BinarySearchTree();
+
+	cur_node->insert(key, value);
+}
---
+++ src/binary_search_tree/binary_search_tree.h
@@ -0,0 +1,35 @@
+#include <string>
+class BinarySearchTree;
+class DataNode {
+	public :
+		DataNode() : value_(NULL) {}
+		~DataNode() { free(value_); }
+		void set_key(std::string key) { key_ = key; } 
+		void set_value(void *value) { value_ = value; }
+		const std::string& get_key() { return key_; }
+		const void* get_value() { return value_; }
+	private:
+		std::string key_;
+		void *value_;
+};
+
+class BinarySearchTree {
+	public:
+		BinarySearchTree() : data_(NULL), left_child(NULL), right_child(NULL) {};
+		~BinarySearchTree() {
+			free(data_);
+			free(left_child);
+			free(right_child);
+		}
+		bool find(const std::string &key);
+		void insert(std::string key, void *value);
+		std::string data() { return data_->get_key(); }
+		bool empty() { return data_ == NULL; }
+		BinarySearchTree *get_lchild() { return left_child; }
+		BinarySearchTree *get_rchild() { return right_child; }
+
+	private:
+		DataNode *data_;
+		BinarySearchTree *left_child;
+		BinarySearchTree *right_child;
+};
